069ad09dc587a485870bf5c5ef2369f469149e91
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2023-11-27 12:52:06 +0000

    Done till the Hero page of the website.

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,25 @@
+"use client";
+import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { useState } from "react";
+const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AuthLinks;
